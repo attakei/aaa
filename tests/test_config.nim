cf1789378e724e5c83a90b0766427a90852db45f
Kazuya Takei <myself@attakei.net>
2020-09-29 23:52:38 +0900

    Fix test environment

--- tests/test_config.nim
+++ tests/test_config.nim
@@ -4,13 +4,14 @@ import "config"
 import "meta"
 
 
-test "Config can save":
-  let saved = getEnv("NIMBLE_TEST_VAR") & DirSep & "test.cfg"
-  var cfg = AConfig()
-  cfg.saveTo(saved)
-  check saved.getFileSize() > 0
+suite "Config in/out":
+  test "Config can save":
+    let saved = getEnv("NIMBLE_TEST_VAR") & DirSep & "test.cfg"
+    var cfg = AConfig()
+    cfg.saveTo(saved)
+    check saved.getFileSize() > 0
 
-test "Config loadings":
-  let src = getEnv("NIMBLE_TEST_DIR") & DirSep & "test_config/regular.cfg"
-  let cfg = loadConfig(src)
-  check cfg.version == meta.version
+  test "Config loadings":
+    let src = getEnv("NIMBLE_TEST_DIR") & DirSep & "testdata/regular.cfg"
+    let cfg = loadConfig(src)
+    check cfg.version == meta.version
