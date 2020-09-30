a35b0514a13fc604e1f298babfc7fe739069c0fc
Kazuya Takei <myself@attakei.net>
2020-09-30 09:10:46 +0900

    Implement fetching questions/replies

--- src/services/teratail/api.nim
+++ src/services/teratail/api.nim
@@ -1,15 +1,28 @@
 ## API handler module
+import httpclient
+import json
+import times
+import "./types"
+
 
 const
   PRODUCTION_ENDPOINT = "https://teratail.com/api/v1"
+  FORMAT_OF_TIMESTMAP = initTimeFormat("yyyy-MM-dd HH:mm:ss")
 
 type
   ApiClient* = ref object
     ## API control client
     endpoint: string  ## URL endpont (base)
+  ApiResponse*[T] = ref object
+    ## Package of response component
+    page: int
+    totalPage: int
+    items: seq[T]
 
 
 proc endpoint*(self: ApiClient): string {.inline.} = self.endpoint
+
+proc items*[T](self: ApiResponse[T]): seq[T] {.inline.} = self.items 
 
 
 proc newApiClient*(): ApiClient =
@@ -21,3 +34,35 @@ proc newApiClient*(endpoint: string): ApiClient =
   result = ApiClient(
     endpoint: endpoint,
   )
+
+proc request(self: ApiClient, urlPath: string): JsonNode =
+  let client = newHttpClient()
+  let resp = client.request(self.endpoint & urlPath, HttpGet)
+  return parseJson(resp.body)
+
+
+proc getQuestions*(self: ApiClient, displayName: string): ApiResponse[Question] =
+  result = ApiResponse[Question](items: @[])
+  let urlPath = "/users/" & displayName & "/questions"
+  let data = self.request(urlPath)
+  result.page = data["meta"]["page"].getInt()
+  result.totalPage = data["meta"]["total_page"].getInt()
+  for q in data["questions"]:
+    result.items.add(newQuestion(
+      q["id"].getInt(),
+      q["title"].getStr(),
+      times.parse(q["created"].getStr(), FORMAT_OF_TIMESTMAP),
+    ))
+
+
+proc getReplies*(self: ApiClient, displayName: string): ApiResponse[Reply] =
+  result = ApiResponse[Reply](items: @[])
+  let urlPath = "/users/" & displayName & "/replies"
+  let data = self.request(urlPath)
+  result.page = data["meta"]["page"].getInt()
+  result.totalPage = data["meta"]["total_page"].getInt()
+  for q in data["replies"]:
+    result.items.add(newReply(
+      q["question_id"].getInt(),
+      times.parse(q["created"].getStr(), FORMAT_OF_TIMESTMAP),
+    ))
--- tests/test_services_teratail_api.nim
+++ tests/test_services_teratail_api.nim
@@ -9,3 +9,15 @@ suite "API Client":
   test "Change URL":
     let apiClient = newApiClient("http://example.com")
     check apiClient.endpoint == "http://example.com"
+
+suite "ApiClient.getXxx":
+  let endpoint = "https://private-anon-d708d0e3a4-teratailv1.apiary-mock.com/api/v1"
+  let apiClient = newApiClient(endpoint)
+
+  test "questions":
+    let resp = apiClient.getQuestions("teratail")
+    check len(resp.items) == 2
+
+  test "replies":
+    let resp = apiClient.getReplies("teratail")
+    check len(resp.items) == 2
