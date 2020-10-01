a94f695f2008c74d39877c97819f5cb6601412b5
Kazuya Takei <myself@attakei.net>
2020-10-02 03:05:32 +0900

    Pull and show results

--- src/commands/pull.nim
+++ src/commands/pull.nim
@@ -4,14 +4,36 @@
 ## "pull" command collect activites some services of configured.
 ## 
 ## If you want to know arguments in command, see ``main()``
+import options
+import sequtils
+import times
 import "../config"
 import "../core/types/activity"
+import "../services/teratail/action" as teratail_action
 
 
-proc main*(config: string): int =
+proc main*(config: string, date: string = ""): int =
   ## Collect activities from services
   let cfg = loadConfig(config)
   var acts: seq[Activity] = @[]
+
+  var targetDate: DateTime
+  if date == "":
+    targetDate = now()
+    targetDate -= 1.days
+  else:
+    targetDate = parse(date, "yyyy-MM-dd")
+    targetDate.hour = 0
+    targetDate.minute = 0
+    targetDate.second = 0
+  echo("Target is " & targetDate.format("yyyy-MM-dd"))
+
+  if cfg.teratail.isSome:
+    echo "Pull acts from teratail"
+    block:
+      let runner = teratail_action.newTeratailRunner(cfg.teratail.get())
+      acts = concat(acts, runner.fetch(targetDate))
+    echo("OK")
 
   # Display result
   echo($len(acts) & " activities")
---
+++ src/services/teratail/action.nim
@@ -0,0 +1,51 @@
+import options
+import sequtils
+import times
+import "./api"
+import "./config"
+import "./types"
+import "../../core/types/activity"
+
+
+type TeratailRunner* = ref object
+  ## Handle endpoint for tact commands
+  cfg: TeratailConfig
+  apiClient: ApiClient
+
+
+proc newTeratailRunner*(cfg: TeratailConfig): TeratailRunner =
+  let apiClient =
+    if cfg.apiKey.isNone:
+      newApiClient()
+    else:
+      newApiClient(
+        DEFAULT_ENDPOINT,
+        cfg.apiKey.get(),
+      )
+  result = TeratailRunner(cfg: cfg, apiClient: apiClient)
+
+
+proc fetch*(self: TeratailRunner, targetDate: DateTime): seq[Activity] =
+  ## Fetch activities from teratail.
+  let nextDate = targetDate + 1.days
+
+  # Questions
+  var questions = newSeq[Activity]()
+  block:
+    var resp = self.apiClient.getQuestions(self.cfg.username)
+    questions = concat(questions, resp.items.mapIt(it.toActivity()))
+    while resp.hasNext() and questions[questions.len-1].timestamp > targetDate:
+      var resp = self.apiClient.getQuestions(self.cfg.username, resp.nextPage)
+      questions = concat(questions, resp.items.mapIt(it.toActivity()))
+
+  # Replies
+  var replies = newSeq[Activity]()
+  block:
+    var resp = self.apiClient.getReplies(self.cfg.username)
+    replies = concat(replies, resp.items.mapIt(it.toActivity()))
+    while resp.hasNext() and replies[replies.len-1].timestamp > targetDate:
+      var resp = self.apiClient.getReplies(self.cfg.username, resp.nextPage)
+      replies = concat(replies, resp.items.mapIt(it.toActivity()))
+
+  # Return
+  return concat(questions, replies).filterIt(it.timestamp >= targetDate).filterIt(it.timestamp < nextDate)
