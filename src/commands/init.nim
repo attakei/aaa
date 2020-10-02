5ae2ab843038bf8f8a0c38224f053bcdd27548d2
Kazuya Takei <myself@attakei.net>
2020-10-03 00:19:20 +0900

    Create database in initialize

--- src/commands/init.nim
+++ src/commands/init.nim
@@ -6,7 +6,9 @@
 ## If you want to know arguments in command, see ``main()``
 import os
 import terminal
+import norm/sqlite
 import "../config"
+import "../core/models/activity"
 
 
 template withTerminalStyle(
@@ -21,12 +23,19 @@ template withTerminalStyle(
     setForegroundColor(stdout, fg)
   body
   resetAttributes(stdout)
-  
+
+
+proc initializeDatabase(dbPath: string) =
+  let db = sqlite.open(dbPath, "", "", "")
+  newActivity().createTable(db)
+
 
 proc main*(output: string): int =
   ## Initialize environment
   result = 0  # Always return 0
   addQuitProc(resetAttributes)
+
+  let outputDir = os.absolutePath(output)
 
   # Display welcome message
   withTerminalStyle(fgGreen):
@@ -36,12 +45,12 @@ proc main*(output: string): int =
     echo(r" | || (_| | (__| |_  ")
     echo(r"  \__\__,_|\___|\__| ")
     echo(r"=====================")
-    echo("Start initialize tact configurations.")
-    echo("This configuration will save to '" & output & "'")
+    echo("Start initialize tact.")
+    echo("Files are created into '" & output & "'")
   echo("")
 
   # Check if config is exists.
-  let outputPath = os.absolutePath(output)
+  let outputPath = outputDir & DirSep & "tact.cfg"
   if not parentDir(outputPath).existsDir():
     withTerminalStyle(fgRed):
       echo("Output directory must be already exists.")
@@ -52,7 +61,8 @@ proc main*(output: string): int =
       return 1
 
   # Save to file
-  var cfg = createConfig()
+  var cfg = createConfig(outputDir)
+  initializeDatabase(cfg.database)
   cfg.saveTo(outputPath)
   withTerminalStyle(fgGreen):
     echo("Finished initialize!!")
